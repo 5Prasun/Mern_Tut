f9b588684b0f0a137e81ab8f935805d6ece57e8e
Prasun  Dandapat <129579131+5Prasun@users.noreply.github.com>
2025-06-19 10:05:00 +0530

    fix: correct mongoose connection syntax and improve error handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose';
+import { DB_name } from './constant'; 
+
+import express from 'express';
+
 
 (async()=>{
     try{
-        mongoose.connect
+         awaitmongoose.connect(`${process.env.MONGO_URI}/${DB_name}`)
+
+         app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
     }
     catch(error){
         console.error("Error",error)
-        throw err
+        throw error
     }
 })()
